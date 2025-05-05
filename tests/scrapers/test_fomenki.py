b1f3537506b2806342f5346fce999e2b4fd0a3ff
Sergey Ilyev <ilev.sv@phystech.edu>
2025-05-05 20:37:00 +0300

    Update tests

--- tests/scrapers/test_fomenki.py
+++ tests/scrapers/test_fomenki.py
@@ -2,7 +2,7 @@ import datetime
 
 import pytest
 
-from scrapers.fomenki import FomenkiScraper
+from src.scrapers.fomenki import FomenkiScraper
 
 
 @pytest.fixture
@@ -25,8 +25,6 @@ async def test_get_performances(fomenki_scraper, html_content):
     performances = await fomenki_scraper.parse_repertoire(fonemki_html)
     assert isinstance(performances, list), "Expected a list of performances"
     assert len(performances) > 0, "Expected at least one performance"
-    assert performances[0].performance_id == 0, "Expected performance_id to be 0"
-    assert performances[0].stage_id == 1, "Expected stage_id to be 1"
     assert performances[0].title == "Приречная страна"
     assert performances[0].datetime == datetime.datetime(
         year=2025, month=5, day=1, hour=14, minute=0
