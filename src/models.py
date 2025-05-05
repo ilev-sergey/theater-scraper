6b73a9d2f4a0bbc93f673b6a2c6b7a6bd443ca67
Sergey Ilyev <ilev.sv@phystech.edu>
2025-05-05 18:03:39 +0300

    Define relationships between models

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 import datetime
 from enum import Enum
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class TheaterName(str, Enum):
@@ -16,18 +16,25 @@ class Theater(SQLModel, table=True):
     full_name: str | None = None
     url: str | None = None
 
+    stages: list["Stage"] = Relationship(back_populates="theater")
+
 
 class Stage(SQLModel, table=True):
     stage_id: int | None = Field(default=None, primary_key=True)
     name: str
-    theater_id: int
+    theater_id: int = Field(foreign_key="theater.theater_id")
     address: str | None = None
+
+    theater: "Theater" = Relationship(back_populates="stages")
+    performances: list["Performance"] = Relationship(back_populates="stage")
 
 
 class Performance(SQLModel, table=True):
     performance_id: int | None = Field(default=None, primary_key=True)
     title: str
-    stage_id: int
+    stage_id: int = Field(foreign_key="stage.stage_id")
     datetime: datetime.datetime
     director: str | None = None
     author: str | None = None
+
+    stage: "Stage" = Relationship(back_populates="performances")
